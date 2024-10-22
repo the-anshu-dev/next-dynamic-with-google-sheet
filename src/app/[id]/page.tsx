227d9ba2303624ad771a6a26534f1cd59604d352
anshu_dev06 <developer6.wallicon@gmail.com>
2024-10-22 17:04:33 +0530

    build

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,75 +1,5 @@
  
 
-// // app/[id]/page.tsx
-// 'use client'
-// import { useParams } from 'next/navigation';
-// import { useEffect, useState } from 'react';
-
-// interface Props {
-//   params: {
-//     id: string;
-//   };
-// }
-
-// const SHEET_ID = '1tjgqBAEq9U3ZQjV5sV9L6u9pkq3um1V8xUjXlAa5IN8';
-// const API_KEY = 'AIzaSyBH3DBh6KjH1vUWoMgc6Bl9SOs_iP-NSRw';
-// const SHEET_NAME = 'Home';
-
-// export default function Page( ) {
-//   // const { id } = params;
-//   const params = useParams(); // Use useParams to access the params object
-//   const { id } = params as { id: string };
-//   const [rowData, setRowData] = useState<string[] | null>(null);
-
-//   useEffect(() => {
-//     const fetchRowData = async () => {
-//       try {
-//         const response = await fetch(
-//           `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${SHEET_NAME}?key=${API_KEY}`
-//         );
-//         const result = await response.json();
-        
-//         console.log("resultresultresult ===========> ",result?.values);
-
-//       // const rowIndex = 
-//       result?.values?.map((item:any)=>{
-//         if(item[0]===id){
-//           setRowData(item)
-//         }
-//       })
-        
-//       } catch (error) {
-//         console.error('Error fetching row data:', error);
-//         setRowData(null);
-//       }
-//     };
-
-//     fetchRowData();
-//   }, [id]);
-
-//   return (
-//     <div>
-//       {rowData ? (
-//         <div>
-//           <h1>Data for Row  {id}</h1>
-//           <ul>
-//             {rowData.map((data, index) => (
-//               <li key={index}>{data}</li>
-//             ))}
-//           </ul>
-//         </div>
-//       ) : (
-//         <p>No data available for this row</p>
-//       )}
-//     </div>
-//   );
-// }
-
-
-
-
-
-
 
 
 
@@ -94,12 +24,12 @@ export default function Page() {
         const response = await fetch(
           `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${SHEET_NAME}?key=${API_KEY}`
         );
-        const result = await response.json();
+        const result: { values: string[][] } = await response.json();
 
         console.log("resultresultresult ===========> ", result?.values);
 
         // Find and set the row data based on the `id`
-        const matchingRow = result?.values?.find((item: any) => item[0] === id);
+        const matchingRow = result?.values?.find((item: string[]) => item[0] === id);
         if (matchingRow) {
           setRowData(matchingRow);
         } else {
